9aaf1ace56d59659b4cd091ef288ad665e4627cd
ΛYUSH <siddhantsinghchauhan1@gmail.com>
2025-03-22 18:14:00 +0530

    numpy loop

---
+++ python_intermediate/loops/loop_numpy.py
@@ -0,0 +1,10 @@
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for i in np_height:
+    print(i, 'inches')
+
+# For loop over np_baseball
+for i in np.nditer(np_baseball):
+    print(i)
