850ae99f301e2437f741a5642cb859b7087d44e0
ΛYUSH <siddhantsinghchauhan1@gmail.com>
2025-04-16 11:38:04 +0530

    sampling deals

---
+++ stats in python/random_number_probab/sampling_deals.py
@@ -0,0 +1,9 @@
+# Set random seed
+np.random.seed(24)
+
+# Sample 5 deals without replacement
+sample_without_replacement = amir_deals.sample(5)
+print(sample_without_replacement)
+
+sample_with_replacement = amir_deals.sample(5, replace=True)
+print(sample_with_replacement)
